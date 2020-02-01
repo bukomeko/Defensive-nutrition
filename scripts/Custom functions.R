21136dfc9fa82bccd800dae7db1a871a5f8169d4
Bukomeko <hannington.bukomeko@wur.nl>
2020-02-02 00:05:44 +0300

    function testing interaction

--- scripts/Custom functions.R
+++ scripts/Custom functions.R
@@ -155,3 +155,62 @@
 return(bla_plot)
     
   }
+  
+  
+  myaddterm <- function (object, scope, scale = 0, test = c("none", "Chisq"), 
+                         k = 2, sorted = FALSE, trace = FALSE, ...) 
+  {
+    if (missing(scope) || is.null(scope)) 
+      stop("no terms in scope")
+    if (!is.character(scope)) 
+      scope <- add.scope(object, update.formula(object, scope))
+    if (!length(scope)) 
+      stop("no terms in scope for adding to object")
+    ns <- length(scope)
+    ans <- matrix(nrow = ns + 1L, ncol = 2L, dimnames = list(c("<none>", 
+                                                               scope), c("df", "AIC")))
+    ans[1L, ] <- extractAIC(object, scale, k = k, ...)
+    n0 <- nobs(object, use.fallback = TRUE)
+    env <- environment(formula(object))
+    for (i in seq_len(ns)) {
+      tt <- scope[i]
+      if (trace) {
+        message(gettextf("trying + %s", tt), domain = NA)
+        utils::flush.console()
+      }
+      nfit <- update(object, as.formula(paste("~ . +", tt)), 
+                     evaluate = FALSE)
+      nfit <- try(eval(nfit, envir = env), silent = TRUE)
+      print(summary(nfit))
+      ans[i + 1L, ] <- if (!inherits(nfit, "try-error")) {
+        nnew <- nobs(nfit, use.fallback = TRUE)
+        if (all(is.finite(c(n0, nnew))) && nnew != n0) 
+          stop("number of rows in use has changed: remove missing values?")
+        extractAIC(nfit, scale, k = k, ...)
+      }
+      else NA_real_
+    }
+    dfs <- ans[, 1L] - ans[1L, 1L]
+    dfs[1L] <- NA
+    aod <- data.frame(Df = dfs, AIC = ans[, 2L])
+    o <- if (sorted) 
+      order(aod$AIC)
+    else seq_along(aod$AIC)
+    test <- match.arg(test)
+    if (test == "Chisq") {
+      dev <- ans[, 2L] - k * ans[, 1L]
+      dev <- dev[1L] - dev
+      dev[1L] <- NA
+      nas <- !is.na(dev)
+      P <- dev
+      P[nas] <- MASS:::safe_pchisq(dev[nas], dfs[nas], lower.tail = FALSE)
+      aod[, c("LRT", "Pr(Chi)")] <- list(dev, P)
+    }
+    aod <- aod[o, ]
+    head <- c("Single term additions", "\nModel:", deparse(formula(object)))
+    if (scale > 0) 
+      head <- c(head, paste("\nscale: ", format(scale), "\n"))
+    class(aod) <- c("anova", "data.frame")
+    attr(aod, "heading") <- head
+    aod
+  }
